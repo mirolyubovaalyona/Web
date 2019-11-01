78eff4f2e61e95a0ff44037e0b308202fc236800
mirolyubovaalyona <56440546+mirolyubovaalyona@users.noreply.github.com>
2019-11-02 01:50:03 +1000

    +
    + 11 + 1 3

---
+++ 13.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WEB1
+{
+    class _13 {
+        static void Main(string[] args) {
+            int l, max, min;
+            string s = Console.ReadLine();
+            string[] tmp = s.Split(' ');
+            l = int.Parse(tmp[0]);
+            max = int.Parse(tmp[2]);
+            min = int.Parse(tmp[1]);
+            int[] a = new int[l];
+            Random rand = new Random();
+            for (int i = 0; i < l; i++)
+            {
+                a[i] = rand.Next(min, max);
+                Console.Write(a[i] + " ");
+            }
+        }
+    }
+}

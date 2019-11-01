d4c395405bd1cf750d609988385816b55089f830
mirolyubovaalyona <56440546+mirolyubovaalyona@users.noreply.github.com>
2019-11-02 09:36:04 +1000

    + 12
    +12

---
+++ 12.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*При старте приложения, в консоле юзера просят выбрать одну из операций(+ - * /).
+Потом два числа.
+Потом вывести результат операции.
+И повторно запросить выбор операции или предложить ввести exit для выхода.
+Также нужно добавить обработку ошибок.*/
+
+
+namespace WEB1
+{
+    class _12
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("'+' or '-' or '*' or '/' or 'exit'");
+            int a, b;
+            string str = "";
+            while (str != "exit")
+            {
+                str = Console.ReadLine();
+                if (str == "+")
+                {
+                    a = Convert.ToInt32(Console.ReadLine());
+                    b = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("a + b = ");
+                    Console.WriteLine( a + b);
+                }
+                else if (str == "-")
+                {
+                    a = Convert.ToInt32(Console.ReadLine());
+                    b = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("a - b = ");
+                    Console.WriteLine(a - b);
+                }
+                else if (str == "*")
+                {
+                    a = Convert.ToInt32(Console.ReadLine());
+                    b = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("a * b = ");
+                    Console.WriteLine(a * b);
+                }
+                else if (str == "/")
+                {
+                    a = Convert.ToInt32(Console.ReadLine());
+                    b = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("a / b = ");
+                    Console.WriteLine(a / b);
+                }
+                else if (str != "exit")
+                {
+                    Console.WriteLine("'+' or '-' or '*' or '/' or 'exit'");
+                    str = Console.ReadLine();
+                }
+            }
+        }
+    }
+}

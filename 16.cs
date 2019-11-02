024459e06efeb7f2f21946027dae0c49f9afed16
mirolyubovaalyona <56440546+mirolyubovaalyona@users.noreply.github.com>
2019-11-02 10:15:08 +1000

    Create 16.cs
    +16

---
+++ 16.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WEB1
+{
+    class _16
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("длинный текст?");
+            string str = Console.ReadLine();
+            Console.WriteLine("длинный текст: {0}", str);
+        }
+    }
+}

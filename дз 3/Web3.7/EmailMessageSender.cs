81143e8704122d02e3d7ba2a620611cfa69e85f7
mirolyubovaalyona <niroliubova.aa@students.dvfu.ru>
2020-04-11 12:13:11 +1000

    1
    1

---
+++ дз 3/Web3.7/EmailMessageSender.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Web3._7
+{
+    public class EmailMessageSender :IMessageSender
+    {
+        HttpContext context;
+        public EmailMessageSender(HttpContext contextt)
+        {
+            context = contextt;
+        }
+        public async void Send()
+        {
+            if (context.Session.Keys.Contains("text"))
+                await context.Response.WriteAsync($"{context.Session.GetString("text")}");
+            else
+            {
+                context.Session.SetString("text", "tttttt1");
+                await context.Response.WriteAsync("text empty");
+            }
+        }
+    }
+}
---
+++ дз 3/Web3.7/SmsMessageService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Web3._7
+{
+    public class SmsMessageSender : IMessageSender
+    {
+        HttpContext context;
+        public SmsMessageSender(HttpContext contextt)
+        {
+            context = contextt;
+        }
+
+        public async void Send()
+        {
+            if (context.Request.Cookies.ContainsKey("text"))
+            {
+                string text = context.Request.Cookies["text"];
+                await context.Response.WriteAsync($"{text}");
+            }
+            else
+            {
+                context.Response.Cookies.Append("name", "Totearm");
+                await context.Response.WriteAsync("text empty");
+            }
+        }
+    }
+}
